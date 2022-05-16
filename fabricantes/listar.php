81c17d18ab8a652426c9f12c941480219cdd74cf
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-05-16 09:15:18 -0300

    Update listar.php

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -1,3 +1,16 @@
+<!-- Criando a conexão com o MySQL (API/Driver de conexão) -->
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas";
+ $conexao = new PDO(
+    "mysql:host=$servidor; dbname=$banco; charset=utf8",$usuario,$senha
+);
+// Habilitando a verificação de erros
+$conexao->setAttribute(PDO::ATTR_ERRMODE, //constante de erros em geral
+PDO::ERRMODE_EXCEPTION );//constante de exceções de erros
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +20,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Fabricantes | SELECT</h1>
+        <hr>
+        <h2>Lendo e carregando todos os fabricantes </h2>
+        <table>
+            <caption>Lista de Fabricantes</caption>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+        
+    </div>
 </body>
 </html>
