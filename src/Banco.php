8673e51cdb3b29143f2eed3d92c767adb3941eef
GabrielG9z <genovezgabriel2012@gmail.com>
2022-07-06 13:11:52 -0300

    updat

--- src/Banco.php
+++ src/Banco.php
@@ -8,11 +8,11 @@ use PDO, Exception;
 abstract class Banco {
     /* Propriedades/Atributos de acesso ao servidor de Banco de Dados ((private) pois só será acessado nesta classe.) O (static) serve pra acessar recursos sem que sejam um objeto.*/
     
-    private static string $server = "localhost";
-    private static string $user = "root";
-    private static string $senha = "";
+    private static string $servidor = "localhost";
     private static string $banco = "vendas";
-
+    private static string $usuario = "root";
+    private static string $senha = "";
+    
     /* A barra invertida (\) faz com que a referência PDO vá na raiz da linguagem, e não na classe 
     private static \PDO $conexao;*/
 
@@ -20,21 +20,21 @@ abstract class Banco {
 
     /* Método de conexão ao banco */
 
-    public static function conecta():PDO {
+    public static function conecta(): PDO {
         try {
             /* Acessando e concatenando as propriedades estáticas com o recurso (self::) self permite acessar recursos estáticos da própria Classe.*/
 
-            self::$conexao = new PDO(
-                "mysql:host=".self::$server.";dbname=".self::$banco.";charset=utf8", self::$user, self::$senha);  
+            self :: $conexao = new PDO(
+                "mysql:host= ". self :: $servidor ." ;dbname= ". self :: $banco .";charset=utf8", self :: $usuario , self :: $senha);  
                               
-            self::$conexao->setAttribute(PDO::ATTR_ERRMODE, 
-            PDO::ERRMODE_EXCEPTION );
+            self :: $conexao->setAttribute(PDO :: ATTR_ERRMODE, 
+            PDO :: ERRMODE_EXCEPTION );
 
         }
          catch (Exception $erro) {
             die("Deu ruim: ".$erro->getMessage());
         }
-        return self::$conexao;
+        return self :: $conexao;
     }
 }
 
--- src/conecta.php
+++ src/conecta.php
@@ -1,19 +1,19 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "vendas";
+//$servidor = "localhost";
+//$usuario = "root";
+//$senha = "";
+//$banco = "vendas";
 
-try {
+//try {
     //Criando a conexão com o MySQL (API/Driver de conexão)
- $conexao = new PDO(
-    "mysql:host=$servidor; dbname=$banco; charset=utf8",$usuario,$senha);
+// $conexao = new PDO(
+ //   "mysql:host=$servidor; dbname=$banco; charset=utf8",$usuario,$senha);
 
     // Habilitando a verificação de erros
-$conexao->setAttribute(PDO::ATTR_ERRMODE, //constante de erros em geral
-PDO::ERRMODE_EXCEPTION );//constante de exceções de erros
+///$conexao->setAttribute(PDO::ATTR_ERRMODE, //constante de erros em geral
+//PDO::ERRMODE_EXCEPTION );//constante de exceções de erros
 
 
-    } catch(Exception $erro) {
-        die("Erro: " .$erro->getMessage());
-    }
+  //  } catch(Exception $erro) {
+  //      die("Erro: " .$erro->getMessage());
+  //  }
